ceea86be3c3fe5036b0e2175ee7abc2e34def7fd
Keola Morse <keolamorse@gmail.com>
2019-01-24 22:24:08 -0800

    library

--- src/components/Library.js
+++ src/components/Library.js
@@ -8,17 +8,17 @@ class Library extends Component {
     this.state = { albums: albumData };
   }
   render() {
-   return ( 
-     <section className='library'>
-       {
-          this.state.albums.map( (album, index) => 
-          <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
-               <div>{album.title}</div>
-               <div>{album.artist}</div>
-               <div>{album.songs.length} songs</div>
-            </Link>
-          )
+    return ( 
+      <section className='library'>
+        {
+          this.state.albums.map( (album, index) =>     
+              <Link to={`/album/${album.slug}`} key={ index }>
+                <img src={album.albumCover} alt={album.title} />
+                <div>{album.title}</div>
+                <div>{album.artist}</div>
+                <div>{album.songs.length} songs</div>
+              </Link>
+            )
         }
      </section>
     );
